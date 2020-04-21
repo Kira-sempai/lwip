55287af0167e4220e2372d838fd200fc0725a990
Andrey.Vinogradov <andrey.vinogradov@teplomonitor.ru>
2020-04-21 20:21:28 +0300

    src/include/lwip/apps/fs_custom_files.h: disable extra declaration

--- src/include/lwip/apps/fs_custom_files.h
+++ src/include/lwip/apps/fs_custom_files.h
@@ -48,7 +48,7 @@ int fs_open_custom(struct fs_file *file, const char *name);
 void fs_close_custom(struct fs_file *file);
 int fs_read_custom(struct fs_file *file, char *buffer, int count);
 
-char * getETagHeader(fs_file_extension * const pextension);
+//char * getETagHeader(fs_file_extension * const pextension);
 
 #ifdef __cplusplus
 }
