cd3cd32b4f6d33ea0e9a9ec5d4675c03595a8ee8
Andrey.Vinogradov <andrey.vinogradov@teplomonitor.ru>
2022-05-11 16:59:58 +0300

    lwip/lwipopts.h: add defines for AT32 case

--- src/include/lwip/lwipopts.h
+++ src/include/lwip/lwipopts.h
@@ -250,6 +250,14 @@ void  memt_free (void*);
 
 #define HTTP_MEM_RESERVE (6*1024)
 
+#elif defined(MCU_GROUP_IS_AT32F4)
+// buf size divided by 2
+#define HTTP_MEMUSE_LEVEL_1 (12*1024)
+// buf size divided by 4
+#define HTTP_MEMUSE_LEVEL_2 (16*1024)
+
+#define HTTP_MEM_RESERVE (6*1024)
+
 #else
 #error "MCU is not defined"
 #endif
