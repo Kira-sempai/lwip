a338e606b7e19c9986ace402604df51122bdb9e0
Andrey.Vinogradov <andrey.vinogradov@teplomonitor.ru>
2022-08-02 20:45:50 +0300

    Revert "src/include/lwip/lwipopts.h: change some options"
    This reverts commit 05f9a1c9614c77826b1cdc9854233731510242fc.
    
    # Conflicts:
    #	src/include/lwip/lwipopts.h

--- src/include/lwip/lwipopts.h
+++ src/include/lwip/lwipopts.h
@@ -9,6 +9,13 @@
 #include <stdlib.h>
 #include <hardware/mcu.h>
 
+// --- Memory ---
+#define MEM_LIBC_MALLOC 1
+#define MEMP_MEM_MALLOC 1
+//#define mem_free osFree
+//#define mem_malloc osAlloc
+//#define mem_calloc osCAlloc
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -27,32 +34,8 @@ void  memt_free (void*);
 #define mem_clib_malloc memt_malloc
 #define mem_clib_calloc memt_calloc
 
-/*
-  ------------------------------------
-  -------------- NO SYS --------------
-  ------------------------------------
-*/
-#ifdef CFG_THREADED_LWIP
-#define NO_SYS 0
-#else
-#define NO_SYS 1
-#endif
 
-#define SYS_LIGHTWEIGHT_PROT (!NO_SYS)
-
-
-/*
-   ------------------------------------
-   ---------- Memory options ----------
-   ------------------------------------
-*/
-
-#define MEM_LIBC_MALLOC 1
-#define MEMP_MEM_MALLOC 1
-//#define mem_free osFree
-//#define mem_malloc osAlloc
-//#define mem_calloc osCAlloc
-
+/* ---------- Memory options ---------- */
 /* MEM_ALIGNMENT: should be set to the alignment of the CPU for which
    lwIP is compiled. 4 byte alignment -> define MEM_ALIGNMENT to 4, 2
    byte alignment -> define MEM_ALIGNMENT to 2. */
@@ -60,175 +43,17 @@ void  memt_free (void*);
 
 //#define MEM_SIZE                        (16384*2)
 #define MEM_SIZE                        16384
-/*
-   ------------------------------------------------
-   ---------- Internal Memory Pool Sizes ----------
-   ------------------------------------------------
-*/
-
-#define MEMP_NUM_RAW_PCB                30
-
-#define MEMP_NUM_TCP_PCB                1
-#define MEMP_NUM_TCP_PCB_LISTEN         1
-#define MEMP_NUM_TCP_SEG                8
-
-//#define PBUF_POOL_SIZE                  32 // with NO_SYS=0, might need to increase this to 32 to avoid deadlocks
-#define PBUF_POOL_SIZE                  8
-
-
-/*
-   ----------------------------------
-   ---------- DHCP options ----------
-   ----------------------------------
-*/
-
-#define LWIP_DHCP 1
-
-/**
- * LWIP_DHCP_DOES_ACD_CHECK==1: Perform address conflict detection on the dhcp address.
- */
-#define LWIP_DHCP_DOES_ACD_CHECK        0
-
-#define LWIP_DHCP_BOOTP_FILE 0
-
-/*
-   ----------------------------------
-   ---------- DNS options -----------
-   ----------------------------------
-*/
-#define LWIP_DNS 1
-
-/** DNS maximum number of entries to maintain locally. */
-#define DNS_TABLE_SIZE 2
-
-/** DNS maximum host name length supported in the name table. */
-#define DNS_MAX_NAME_LENGTH             128
-
-/*
-   ---------------------------------
-   ---------- UDP options ----------
-   ---------------------------------
-*/
-
-#define LWIP_UDP 1
-
-
-/*
-   ---------------------------------
-   ---------- TCP options ----------
-   ---------------------------------
-*/
-#ifdef CFG_MODULE_NETWORK
-#define LWIP_TCP 1
-#else
-#define LWIP_TCP 0
-#endif
-
-#define TCP_MSS                         1460
-
-
-#if defined(MCU_GROUP_IS_STM32F1)
-#define TCP_SND_BUF						(6*TCP_MSS)
-#elif defined(MCU_GROUP_IS_STM32F2)
-#define TCP_SND_BUF						(6*TCP_MSS)
-#elif defined(MCU_GROUP_IS_STM32F7)
-#define TCP_SND_BUF						(6*TCP_MSS)
-#elif defined(MCU_GROUP_IS_AT32F4)
-#define TCP_SND_BUF						(6*TCP_MSS)
-#endif
-
-//#define TCP_SND_QUEUELEN                30
-
-#define TCP_LISTEN_BACKLOG              1
 
 #ifdef CFG_WIFI_API
 #define LWIP_ALTCP 1
 #endif
 
+#define TCP_MSS                         1460
 #define TCP_OVERSIZE                    0
 
-#ifdef CFG_SWK
-#define TCP_TMR_INTERVAL                250
-#define TCP_SLOW_INTERVAL               500
-#else
-#define TCP_TMR_INTERVAL                500
-#endif
-
-/*
-   ------------------------------------------------
-   ---------- Network Interfaces options ----------
-   ------------------------------------------------
-*/
-
-#define LWIP_NETIF_STATUS_CALLBACK 1
-
-#define LWIP_NETIF_LINK_CALLBACK 1
-
-/*
-   ----------------------------------------------
-   ---------- Sequential layer options ----------
-   ----------------------------------------------
-*/
-#define LWIP_NETCONN         (!NO_SYS)
-
-/*
-   ------------------------------------
-   ---------- Socket options ----------
-   ------------------------------------
-*/
-#define LWIP_SOCKET          0
-
-/*
-   ----------------------------------------
-   ---------- Statistics options ----------
-   ----------------------------------------
-*/
-#define LWIP_STATS              1
-#define LWIP_STATS_DISPLAY      0
-
-#if LWIP_STATS
-#define LINK_STATS              1
-#define IP_STATS                1
-#define ICMP_STATS              1
-#define IGMP_STATS              0
-#define IPFRAG_STATS            0
-#define UDP_STATS               0
-#define TCP_STATS               1
-#define MEM_STATS               0
-#define MEMP_STATS              0
-#define PBUF_STATS              0
-#define SYS_STATS               0
-#endif /* LWIP_STATS */
-
-/*
-   --------------------------------------
-   ---------- Checksum options ----------
-   --------------------------------------
-*/
-
-#define CHECKSUM_GEN_IP                 0
-#define CHECKSUM_GEN_UDP                0
-#define CHECKSUM_GEN_TCP                0
-#define CHECKSUM_CHECK_IP               0
-#define CHECKSUM_CHECK_UDP              0
-#define CHECKSUM_CHECK_TCP              0
-
-
-/*
-   ---------------------------------------
-   ---------- Hook options ---------------
-   ---------------------------------------
-*/
-
-#define LWIP_HOOK_FILENAME <lwip/lwip_hooks.h>
-
-/*
-   ---------------------------------------
-   ---------- HTTP options ---------------
-   ---------------------------------------
-*/
-
 #if defined(MCU_GROUP_IS_STM32F1)
+
+#define TCP_SND_BUF						(2*TCP_MSS)
 // buf size divided by 2
 #define HTTP_MEMUSE_LEVEL_1 (12*1024)
 // buf size divided by 4
@@ -237,6 +62,8 @@ void  memt_free (void*);
 #define HTTP_MEM_RESERVE (4*1024)
 
 #elif defined(MCU_GROUP_IS_STM32F2)
+
+#define TCP_SND_BUF						(6*TCP_MSS)
 // buf size divided by 2
 #define HTTP_MEMUSE_LEVEL_1 (12*1024)
 // buf size divided by 4
@@ -245,6 +72,8 @@ void  memt_free (void*);
 #define HTTP_MEM_RESERVE (8*1024)
 
 #elif defined(MCU_GROUP_IS_STM32F7)
+
+#define TCP_SND_BUF						(6*TCP_MSS)
 // buf size divided by 2
 #define HTTP_MEMUSE_LEVEL_1 (12*1024)
 // buf size divided by 4
@@ -338,19 +167,32 @@ void  memt_free (void*);
 /* Define this to a function that returns the maximum amount of data to enqueue.*/
 #define HTTPD_MAX_WRITE_LEN(pcb) httpGetMaxWriteLen(pcb)
 
+#define LWIP_HOOK_FILENAME <lwip/lwip_hooks.h>
+
 #define LWIP_HTTPD_FILE_STATE         1
 #define LWIP_HTTPD_FILE_EXTENSION     0
+/**
+ * LWIP_DHCP_DOES_ACD_CHECK==1: Perform address conflict detection on the dhcp address.
+ */
+#define LWIP_DHCP_DOES_ACD_CHECK        0
+
 #define FS_FILE_EXTENSION_T_DEFINED
 typedef struct fs_pextension fs_file_extension;
 
 
 #define HTTPD_ADDITIONAL_CONTENT_TYPES {"gz", HTTP_HDR_HTMLZ}
 
-/** Set this to 1 to support fs_read() to dynamically read file data.
- * Without this (default=off), only one-block files are supported,
- * and the contents must be ready after fs_open().
- */
-#define LWIP_HTTPD_DYNAMIC_FILE_READ  1
+/*
+   ----------------------------------
+   ---------- DNS options -----------
+   ----------------------------------
+*/
+/** DNS maximum number of entries to maintain locally. */
+#define DNS_TABLE_SIZE 2
+
+/** DNS maximum host name length supported in the name table. */
+#define DNS_MAX_NAME_LENGTH             128
+
 
 
 #ifdef SIMULATION
@@ -358,6 +200,49 @@ typedef struct fs_pextension fs_file_extension;
 #else
 #include <hardware/mcu.h>
 
+/* ---------- Statistics options ---------- */
+
+#define LWIP_STATS              1
+#define LWIP_STATS_DISPLAY      0
+
+#if LWIP_STATS
+#define LINK_STATS              1
+#define IP_STATS                1
+#define ICMP_STATS              1
+#define IGMP_STATS              0
+#define IPFRAG_STATS            0
+#define UDP_STATS               1
+#define TCP_STATS               1
+#define MEM_STATS               0
+#define MEMP_STATS              0
+#define PBUF_STATS              0
+#define SYS_STATS               0
+#endif /* LWIP_STATS */
+
+#ifdef CFG_THREADED_LWIP
+#define NO_SYS 0
+#else
+#define NO_SYS 1
+#endif
+
+#define SYS_LIGHTWEIGHT_PROT (!NO_SYS)
+#define LWIP_NETCONN         (!NO_SYS)
+#define LWIP_SOCKET          0
+
+#ifdef CFG_MODULE_NETWORK
+#define LWIP_TCP 1
+#else
+#define LWIP_TCP 0
+#endif
+
+#define LWIP_UDP 1
+#define LWIP_NETIF_LINK_CALLBACK 1
+
+#define LWIP_NETIF_STATUS_CALLBACK 1
+
+#define LWIP_DNS 1
+#define LWIP_DHCP 1
+#define LWIP_DHCP_BOOTP_FILE 0
 
 /** Set this to 1 to support SSI (Server-Side-Includes) */
 #define LWIP_HTTPD_SSI            0
@@ -367,40 +252,83 @@ typedef struct fs_pextension fs_file_extension;
 /** Set this to 1 to support HTTP POST */
 #define LWIP_HTTPD_SUPPORT_POST   1
 
+#define MEM_SIZE                        16384
+#define PBUF_POOL_SIZE                  8 // with NO_SYS=0, might need to increase this to 32 to avoid deadlocks
+
+/**
+ * TCP_LISTEN_BACKLOG: Enable the backlog option for tcp listen pcb.
+ */
+#define TCP_LISTEN_BACKLOG              1
+
+//#define TCP_SND_QUEUELEN                (2 * (TCP_SND_BUF)/(TCP_MSS))
+//#define TCP_WND                         (3 * TCP_MSS)
+
+#define MEMP_NUM_TCP_PCB                1
+#define MEMP_NUM_TCP_PCB_LISTEN         1
+#define MEMP_NUM_TCP_SEG                8
+
+//#define MEMP_NUM_SYS_TIMEOUT            (4 + LWIP_DNS + LWIP_TCP)
+
+//#define ARP_QUEUEING 0
+//#define MEMP_NUM_ARP_QUEUE              10
+
+//#define IP_FRAG 0
+//#define IP_FRAG_USES_STATIC_BUF         0
+
+//#define TCP_QUEUE_OOSEQ                 0
+
 // this block is only used if NO_SYS==0
 #if NO_SYS == 0
 //#define DEFAULT_THREAD_STACKSIZE 0x40
 //#define DEFAULT_THREAD_PRIO  30
 #endif
 
-/*
-   ---------------------------------------
-   ---------- Debugging options ----------
-   ---------------------------------------
-*/
+//#define DEFAULT_RAW_RECVMBOX_SIZE 20
+//#define DEFAULT_UDP_RECVMBOX_SIZE 20
+//#define DEFAULT_TCP_RECVMBOX_SIZE 20
+//#define DEFAULT_ACCEPTMBOX_SIZE 2
+
+//#define TCPIP_MBOX_SIZE 20 // number of entries
+//#define TCPIP_THREAD_STACKSIZE 0x1E0
+//#define TCPIP_THREAD_PRIO    130
+// ------------------------------------
+
+#define CHECKSUM_GEN_IP                 0
+#define CHECKSUM_GEN_UDP                0
+#define CHECKSUM_GEN_TCP                0
+#define CHECKSUM_CHECK_IP               0
+#define CHECKSUM_CHECK_UDP              0
+#define CHECKSUM_CHECK_TCP              0
+
+//#define TCP_TMR_INTERVAL       100  /* The TCP timer interval in milliseconds. */
+
 //#define LWIP_DEBUG
 
 #ifdef LWIP_DEBUG
 #define LWIP_DBG_MIN_LEVEL         LWIP_DBG_LEVEL_ALL
-
-//#define TCP_RST_DEBUG LWIP_DBG_ON
-//#define UDP_DEBUG LWIP_DBG_ON
+#define UDP_DEBUG LWIP_DBG_ON
 //#define MEMP_DEBUG LWIP_DBG_ON
 //#define PBUF_DEBUG LWIP_DBG_ON
 //#define SYS_DEBUG LWIP_DBG_ON
 //#define TCP_INPUT_DEBUG LWIP_DBG_ON
-#define TCP_OUTPUT_DEBUG LWIP_DBG_ON
+//#define TCP_OUTPUT_DEBUG LWIP_DBG_ON
 //#define TCP_DEBUG LWIP_DBG_ON
 //#define TIMERS_DEBUG LWIP_DBG_OFF
 //#define DHCP_DEBUG LWIP_DBG_ON
-//#define TCP_QLEN_DEBUG      LWIP_DBG_ON
-#define HTTPD_DEBUG         LWIP_DBG_ON
+//#define TCP_QLEN_DEBUG                  LWIP_DBG_ON
+//#define HTTPD_DEBUG         LWIP_DBG_ON
+//#define LWIP_STATS_DISPLAY 1
 #endif
 
 #include <stdio.h>
 
 #define LWIP_RAND _rand
 
+/** Set this to 1 to support fs_read() to dynamically read file data.
+ * Without this (default=off), only one-block files are supported,
+ * and the contents must be ready after fs_open().
+ */
+#define LWIP_HTTPD_DYNAMIC_FILE_READ  1
 
 #include <platform.h>
 #define SYS_ARCH_DECL_PROTECT(x) CRITICAL_STORE
